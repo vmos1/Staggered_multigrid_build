46c477c33cb3ac1c6f6cbb43a361cccb4afe5757
Venkitesh Ayyar <venkitesh@home3.ccs.ornl.gov>
2022-12-01 12:35:06 -0500

    bug fixes for summit build

--- build_summit/install_scripts/build_milc2.sh
+++ build_summit/install_scripts/build_milc2.sh
@@ -3,7 +3,8 @@ date
 
 #############
 ## Source environment
-source ${BUILD_DIR}/setup_env_summit.sh
+script_loc=$BUILD_DIR/install_scripts
+source $script_loc/setup_env_summit.sh
 
 ############
 ## Build QUDA
@@ -12,13 +13,13 @@ QUDA_BUILD=${BUILD_DIR}/QUDA/build
 
 ## Build MILC
 
-MILCDIR=${BUILD_DIR}/MILC/milc_qcd
+MILCDIR=${BUILD_DIR}/milc_qcd
 
 cd ${MILCDIR}/ks_spectrum && cp ${MILCDIR}/Makefile ${MILCDIR}/ks_spectrum
 export PATH_TO_CUDA=/sw/summit/cuda/11.0.3 # NEED TO MAKE THIS MORE ROBUST TO DIFFERENT CUDA VERSIONS, ASSUMES 11.0.3
 export USQCD_BUILD=${QUDA_BUILD}/usqcd
 
-PATH_TO_CUDA=$PATH_TO_CUDA PATH_TO_QUDA=$QUDA_BUILD PATH_TO_QMP=$USQCD_BUILD PATH_TO_QIO=$USQCD_BUILD POWER9=1 MULTIGRID=1 ./compile_ks_spectrum_quda.sh
+PATH_TO_CUDA=$PATH_TO_CUDA PATH_TO_QUDA=$QUDA_BUILD PATH_TO_QMP=$USQCD_BUILD PATH_TO_QIO=$USQCD_BUILD POWER9=1 MULTIGRID=1 $MILCDIR/ks_spectrum/./compile_ks_spectrum_hisq_quda.sh
 
 cd $BUILD_DIR
 date
