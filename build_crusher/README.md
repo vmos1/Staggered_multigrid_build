98efe43b1a2d9ecb3c8fcfee7b8e98cd040d7dad
Venkitesh <vayyar@bu.edu>
2023-02-03 08:34:44 -0500

    clean up for crusher code

--- build_crusher/README.md
+++ build_crusher/README.md
@@ -1,11 +1,13 @@
-# Instructions for building and running Staggered Multigrid on Crusher
-We need to build QUDA and MILC in sequence.  QUDA can be built with a single script. Due to the need to edit some files in MILC after download, it is built in two steps.
+# Instructions for building and running Staggered Multigrid on Crusher and Frontier supercomputers
+We need to build QUDA and MILC in sequence.  QUDA and MILC can each be built with a single script. 
 The procedure is described below.
 ## **Generate build scripts**: 
 You need 2 directories: ```build_dir``` and ```run_dir```.
-- **Using the jupyter notebook**  :
-  - Edit the folder names in `dict_pars['build_dir']` and `dict_pars['run_dir']` to point to the appropriate locations.
-  - Run the entire jupyter notebook [build_crusher/build_install_scripts_crusher.ipynb](https://github.com/vmos1/Staggered_multigrid_build/blob/main/build_crusher/build_install_scripts_crusher.ipynb). It will copy all install scripts to the desired location in `build_dir`.
+- **Using the python script**  :
+  - Run the script [build_frontier/build_install_scripts.py](https://github.com/vmos1/Staggered_multigrid_build/blob/main/build_frontier/build_install_scripts.py) as 
+  
+  `python build_install_scripts.py --machine frontier -bdir <build_dir>`. 
+  - It will create <build_dir> and copy all install scripts to the desired location in `build_dir`.
 
  **OR Alternatively**  
 - **Manually copy and edit scripts** : 
@@ -15,25 +17,22 @@ You need 2 directories: ```build_dir``` and ```run_dir```.
 | `export BUILD_DIR=<build_dir>`| Setup paths |
 | `cd $BUILD_DIR` | Enter build directory |
 | ` mkdir QUDA/src install` | Manually create 2 empty directories inside `build_dir`  |
-| `cp -r <repo_dir>/build_crusher/install_scripts $BUILD_DIR/` | Copy the folder [*build_crusher/install_scripts*](https://github.com/vmos1/Staggered_multigrid_build/tree/main/build_crusher/install_scripts) to `<build_dir>/` |
+| `cp -r <repo_dir>/build_frontier/install_scripts $BUILD_DIR/` | Copy the folder [*build_frontier/install_scripts*](https://github.com/vmos1/Staggered_multigrid_build/tree/main/build_frontier/install_scripts) to `<build_dir>/` |
 
 ## **Build instructions**: 
   - ```cd <build_dir>``` 
   - `export BUILD_DIR=$PWD`
   - `./install_scripts/build_quda.sh 2>&1 | tee op_quda.out `
-  - `./install_scripts/build_milc1.sh 2>&1 | tee op_milc1.out `
-  - Make edits to files as listed below: 
-    - In the file [milc_qcd/ks_spectrum/compile_ks_spectrum_hisq_quda.sh](https://github.com/milc-qcd/milc_qcd/blob/develop/ks_spectrum/compile_ks_spectrum_hisq_quda.sh), uncomment lines 45 (for non-NVIDIA GPUs).
-  - ```./install_scripts/build_milc2.sh 2>&1 | tee op_milc2.out ```
+  - `./install_scripts/build_milc.sh 2>&1 | tee op_milc.out `
 
 This should build both QUDA and MILC. 
 
-## Instructions for running Staggered Multigrid on Crusher
+## Instructions for running Staggered Multigrid on Frontier
 
 | Command | Description | 
 | -- | -- |
 | ```cd <run_dir>``` | Enter run directory |
-| ```cp <repo_dir>/build_crusher/sample_input_files/* .```  | Copy the input files from [build_crusher/sample_input_files](https://github.com/vmos1/Staggered_multigrid_build/tree/main/build_crusher/sample_input_files) to the required location |
+| ```cp <repo_dir>/build_frontier/sample_input_files/* .```  | Copy the input files from [build_frontier/sample_input_files](https://github.com/vmos1/Staggered_multigrid_build/tree/main/build_frontier/sample_input_files) to the required location |
 | ```mkdir rand``` | Create directory for storing random numbers | 
 | ```ln -s /gpfs/alpine/proj-shared/lgt104/detar/lat``` | Create sym link for gauge configuration | 
 | Edit the `input*.kpp` and `mgparams*.txt` files | Edit for varying local volume, MG layers, etc. | 
---
+++ build_crusher/build_install_scripts.py
@@ -0,0 +1,230 @@
+# Script to generate scripts for buildig QUDA and MILC for Staggered Multigrid on Summit, Crusher and Frontier supercomputers
+## Example run: python build_install_scripts.py --machine frontier -bdir temp/temp2
+'''Code does the following: 
+Creates parent directory
+3 directories install, install_scripts, QUDA/src
+creates 3 files setup_env.sh, build_quda.sh, build_milc.sh and copies them to install_scripts
+'''
+
+import os
+import subprocess as sp
+import argparse
+import shutil
+
+def f_parse_args():
+    """Parse command line arguments.Only for .py file"""
+    parser = argparse.ArgumentParser(description="Script to generate scripts for buildig QUDA and MILC for Staggered Multigrid on Crusher and Frontier supercomputers", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    add_arg = parser.add_argument
+    
+    add_arg('--machine','-m', type=str, choices=['crusher','frontier'], default='crusher', help='Machine used')
+    add_arg('--build_dir','-bdir', type=str, default='', help='build directory')
+    
+    return parser.parse_args()
+
+
+def f_build_env_script(dict_pars,fname):
+    
+    ### Setup env
+    env_strg='''#!/bin/bash
+#############
+
+module load craype-accel-amd-gfx90a
+module load cray-mpich/{cray_mpich}
+module load cmake
+module load rocm/{rocm}
+module list
+
+export MPICH_ROOT=/opt/cray/pe/mpich/{cray_mpich}
+export MPICH_DIR=${{MPICH_ROOT}}/ofi/crayclang/10.0
+
+## These must be set before running
+
+#export SRCROOT=${{BUILD_DIR}}/../src
+#export BUILDROOT=${{BUILD_DIR}}/build
+export INSTALLROOT=${{BUILD_DIR}}/install
+export GPU_TARGET=gfx90a
+
+MPI_CFLAGS="-I${{MPICH_DIR}}/include -g"
+MPI_LDFLAGS="-g -L${{MPICH_DIR}}/lib -lmpi -L${{MPICH_ROOT}}/gtl/lib -lmpi_gtl_hsa"
+
+export PK_BUILD_TYPE="Release"
+
+export PATH=${{ROCM_PATH}}/bin:${{PATH}}
+export LD_LIBRARY_PATH=${{INSTALLROOT}}/quda/lib:${{MPICH_DIR}}/lib:${{MPICH_ROOT}}/gtl/lib:${{LD_LIBRARY_PATH}}
+
+    '''.format(**dict_pars)
+
+    with open(fname,'w') as f: f.write(env_strg)
+
+
+def f_build_quda_script(dict_pars,fname):
+    
+    quda_strg='''#!/bin/bash
+date
+#############
+## Source environment
+
+script_loc=$BUILD_DIR/install_scripts
+source $script_loc/setup_env.sh
+WORKDIR=$BUILD_DIR
+
+############
+## Build QUDA
+QUDA_SRC=${{WORKDIR}}/QUDA/src/quda
+QUDA_BUILD=${{WORKDIR}}/QUDA/build
+
+pushd .
+if [ ! -d $QUDA_SRC ]
+then
+##  git clone --branch feature/hip-compile-fixes https://github.com/lattice/quda $QUDA_SRC # clone QUDA to the desired directory
+git clone --branch develop https://github.com/lattice/quda $QUDA_SRC # clone QUDA to the desired directory
+
+else
+  cd $QUDA_SRC; git pull; cd ..
+fi
+
+if [ ! -d $QUDA_BUILD ]
+then
+  mkdir $QUDA_BUILD
+else
+  cd $QUDA_BUILD
+  rm -rf *
+fi
+
+export QUDA_GPU_ARCH=gfx90a
+
+cd $QUDA_BUILD
+cmake -DCMAKE_BUILD_TYPE=DEVEL -DQUDA_DIRAC_DEFAULT_OFF=ON -DQUDA_DIRAC_STAGGERED=ON -DQUDA_GAUGE_ALG=ON -DQUDA_GAUGE_TOOLS=ON -DQUDA_DOWNLOAD_USQCD=ON -DQUDA_QIO=ON -DQUDA_QMP=ON -DQUDA_MULTIGRID=ON \
+-DQUDA_MULTIGRID_NVEC_LIST="6,24,48,64,96" \
+-DQUDA_TARGET_TYPE="HIP" \
+-DROCM_PATH=${{ROCM_PATH}} \
+-DGPU_TARGETS="gfx90a" \
+-DCMAKE_CXX_COMPILER="hipcc" \
+-DCMAKE_C_COMPILER="hipcc" \
+-DCMAKE_CXX_FLAGS="${{MPI_CFLAGS}}" \
+-DCMAKE_C_FLAGS="${{MPI_CFLAGS}}" \
+-DCMAKE_EXE_LINKER_FLAGS="${{MPI_LDFLAGS}}" \
+-DCMAKE_SHARED_LINKER_FLAGS="${{MPI_LDFLAGS}}" \
+-DCMAKE_C_STANDARD=99 \
+-DCMAKE_INSTALL_PREFIX=${{INSTALLROOT}}/quda \
+$QUDA_SRC
+
+nice make VERBOSE=1 -j 16 
+make install
+popd
+
+date
+'''.format(**dict_pars)
+
+    with open(fname,'w') as f: f.write(quda_strg)
+
+
+def f_build_milc_script(dict_pars,fname):
+
+    milc_strg='''#!/bin/bash
+#############
+## Source environment
+script_loc=$BUILD_DIR/install_scripts
+source $script_loc/setup_env.sh
+
+##git clone --branch feature/staggMG https://github.com/weinbe2/milc_qcd ${{BUILD_DIR}}/milc_qcd
+git clone --branch develop https://github.com/milc-qcd/milc_qcd ${{BUILD_DIR}}/milc_qcd
+
+
+### Uncomment specific lines from the file milc_qcd/ks_spectrum/compile_ks_spectrum_hisq_quda.sh 
+## for AMD GPUs : uncomment line 45
+sed -i "45 s/#//" milc_qcd/ks_spectrum/compile_ks_spectrum_hisq_quda.sh;
+
+QUDA_INSTALL=${{INSTALLROOT}}/quda
+
+LIBQUDA="-Wl,-rpath ${{QUDA_INSTALL}}/lib -L${{QUDA_INSTALL}}/lib -lquda -D__gfx90a --offload-arch=gfx90a --amdgpu-target=gfx90a -Wl,-rpath=${{ROCM_PATH}}/hiprand/lib -L${{ROCM_PATH}}/hiprand/lib -Wl,-rpath=${{ROCM_PATH}}/rocfft/lib -L${{ROCM_PATH}}/rocfft/lib -lhiprand -lrocfft -Wl,-rpath=${{ROCM_PATH}}/hipblas/lib -L${{ROCM_PATH}}/hipblas/lib -lhipblas -Wl,-rpath=${{ROCM_PATH}}/rocblas/lib -L${{ROCM_PATH}}/rocblas/lib -lrocblas -Wl,-rpath=${{ROCM_PATH}}/hip/lib"
+
+echo $BUILD_DIR/milc_qcd/ks_spectrum
+############ Make ks_spectrum_hisq ##################
+cd $BUILD_DIR/milc_qcd/ks_spectrum
+cp ../Makefile .
+make clean
+
+MY_CC=hipcc \
+MY_CXX=hipcc \
+ARCH="" \
+COMPILER="gnu" \
+OPT="-O3 --offload-arch=gfx90a" \
+CUDA_HOME="" \
+QUDA_HOME=${{QUDA_INSTALL}} \
+WANTQUDA=true \
+WANT_FN_CG_GPU=true \
+WANT_FL_GPU=true \
+WANT_GF_GPU=true \
+WANT_FF_GPU=true \
+WANT_MIXED_PRECISION_GPU=2 \
+PRECISION=2 \
+MPP=true \
+OMP=true \
+WANTQIO=true \
+WANTQMP=true \
+QIOPAR=${{INSTALLROOT}}/quda \
+QMPPAR=${{INSTALLROOT}}/quda \
+LIBQUDA=${{LIBQUDA}} \
+OFFLOAD=HIP \
+CGEOM="-DFIX_NODE_GEOM -DFIX_IONODE_GEOM" \
+KSCGMULTI="-DKS_MULTICG=HYBRID -DMULTISOURCE -DMULTIGRID" \
+CTIME="-DNERSC_TIME -DCGTIME -DFFTIME -DFLTIME -DGFTIME -DREMAP -DPRTIME -DIOTIME" \
+make -j 1 ks_spectrum_hisq
+cd ..
+
+date
+'''.format(**dict_pars)
+        
+    with open(fname,'w') as f: f.write(milc_strg)
+    
+    
+
+if __name__=="__main__":
+    args=f_parse_args()
+         
+    if args.build_dir=='':     args.build_dir=os.getcwd()+'/new_runs_1'
+    print("Build scripts for ",args.machine)
+    print("Build directory: ",args.build_dir)
+    
+    dict_pars={}
+    dict_pars['build_dir']= args.build_dir
+    dict_pars['machine']  = args.machine
+    
+    ## Define cray-mpich and rocm versions to use in setupfile
+    dict_pars['cray_mpich']='8.1.14'
+    dict_pars['rocm']      ='4.5.2'
+    # Create empty directories
+    
+    ## Create upper level directories
+    if os.path.exists(dict_pars['build_dir']):
+        print('Error: Directory '+dict_pars['build_dir']+' exists')
+        print("Please delete the directory or give a different build location")
+        raise SystemExit
+    else:
+        os.makedirs(dict_pars['build_dir']+'/QUDA/src/')
+        os.makedirs(dict_pars['build_dir']+'/install')
+        os.makedirs(dict_pars['build_dir']+'/install_scripts')
+    
+    # Build scripts depending on machine
+    fname1='setup_env.sh'
+    fname2='build_quda.sh'
+    fname3='build_milc.sh'
+    f_build_env_script(dict_pars,fname1)
+    f_build_quda_script(dict_pars,fname2)
+    f_build_milc_script(dict_pars,fname3)
+    
+    # Write files
+    print("Copying scripts to ",args.build_dir+'/install_scripts')
+    for fname in [fname1,fname2,fname3]: 
+        
+        # make executables
+        cmd='chmod +x {0}'.format(fname)
+        op=sp.check_output(cmd,shell=True)
+        # print(op)
+        
+        # move files to location
+        # print(fname,dict_pars['build_dir']+'/install_scripts/'+fname)
+        shutil.move(fname,dict_pars['build_dir']+'/install_scripts/'+fname)
+        
+    
